79953ae81b8a1d600355ca544cb61f073b87cf8b
kiwiinova <95432232+kiwiinova@users.noreply.github.com>
2021-12-02 11:32:41 -0500

    Create Ticketbooth.java

--- Ticketbooth.java
+++ Ticketbooth.java
@@ -1,5 +1,13 @@
-import java.util.Scanner;
-public class Ticketbooth {
 
-	Scanner sc = new Scanner (System.in);
+public class Ticketbooth {	
+	private int Tickets;
+	private  int OPUSCards[];
+	
+	public Ticketbooth()
+	{
+	}
+	
+	public Ticketbooth(int Tickets, int OPUSCards[]) {
+		int OPUSCard[] = null; 
+	}
 }
