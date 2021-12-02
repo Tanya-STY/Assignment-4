de0c39eda50aa007e28a0a37f945b1f688fb1540
kiwiinova <95432232+kiwiinova@users.noreply.github.com>
2021-12-02 13:08:13 -0500

    Update Ticketbooth.java

--- Ticketbooth.java
+++ Ticketbooth.java
@@ -1,13 +1,41 @@
+import java.util.Scanner;
 
-public class Ticketbooth {	
-	private int Tickets;
-	private  int OPUSCards[];
+public class Ticketbooth {
 	
-	public Ticketbooth()
+	private Tickets Ticket;
+	private OPUSCard[] OPUSCards;
+	
+	Scanner sc = new Scanner(System.in);
+	int value = sc.nextInt();
+	
+	public Ticketbooth() {
+		Tickets obj = new Tickets();
+		OPUSCards = new OPUSCard[value];
+	}
+
+	public Ticketbooth(Tickets Ticket, OPUSCard card[]) 
 	{
+		Tickets obj = new Tickets(Ticket);
+		card = null; 
+	}
+
+	public boolean equalValues(Ticketbooth T1, Ticketbooth T2) {
+		if (T1.Ticket.ticketsTotal() == T2.Ticket.ticketsTotal()) {
+			return true;
+		}
+		return false;
 	}
 	
-	public Ticketbooth(int Tickets, int OPUSCards[]) {
-		int OPUSCard[] = null; 
+	public boolean equalNumber(Ticketbooth T1, Ticketbooth T2)
+	{
+		if (T1.Ticket.equals(Ticket) == T2.Ticket.equals(Ticket)) {
+			return true;
+		}
+		return false;
 	}
+	
+	public double totalTicket() {
+		return Ticket.ticketsTotal();
+	}
+	
 }
