593e7386c77134580696701be2352b59d2223e39
kiwiinova <95432232+kiwiinova@users.noreply.github.com>
2021-12-05 09:07:06 -0500

    Update Ticketbooth.java

--- Ticketbooth.java
+++ Ticketbooth.java
@@ -35,9 +35,98 @@ public class Ticketbooth {
 		}
 		return false;
 	}
-	
+	// method that returns the total value of tickets in ticketbooth
 	public double totalTicket() {
 		return Ticket.ticketsTotal();
 	}
 	
+	// method that returns the number of opus cards in ticketbooth
+	public int totalOpusNum() 
+	{
+		return Card.length;
+	}
+	
+	// method that adds a new opus card to a ticketbooth and returns the number of opus cards after the addition
+	
+	public int addOpusCard(OPUSCard cardA)
+	{
+		if (Card.length == 0) 
+		{
+			OPUSCard[] addition = new OPUSCard[1];
+			addition[0] = cardA;
+			Card = addition;
+			return Card.length;
+		}
+		
+		else {
+			OPUSCard[] addition = new OPUSCard[Card.length + 1];
+			for (int i = 0; i < Card.length, i++) {
+				addition[i] = Card[i];
+			}
+			addition[addition.length - 1] = cardA;
+			Card = addition;
+			return Card.length;
+		}
+	}
+	
+	// method that removes an opus card from the ticketbooth, returns true if the removal is succesful
+	
+	public boolean removeCard(int minus)
+	{
+		int count = 0;
+		if (Card.length == 0)
+		{
+			return false;
+		}
+		else {
+			OPUSCard[] addition = new OPUSCard[Card.length-1];
+			for (int i = 0; i < (card.length); i++)
+			{
+				if (i != minus)
+				{
+					addition[count] = Card[i];
+					count ++;
+				}
+			}
+			Card = addition;
+			return true;
+		}
+	}
+	// method to update the expiry month and year of an opus card
+	public void updateExpiry (int year, int month, int y)
+	{
+		Card[i].setExp_Month(month);
+		Card[i].setExp_Year(year);
+	}
+	
+	// Method that adds tickets to ticket booth
+	public double  addTickets (int a, int b, int c, int d, int e) 
+	{
+		Ticket.addTickets(a,b,c,d,e);
+		return ticketsTotal();
+	}
+	// method that verifies if the total value of tickets and the number of opus cards tickets of two ticketbooth objects are equal
+	public boolean equalAll(Ticketbooth one, Ticketbooth two) {
+		if (!equalValues(one,two)) 
+		{
+			return false;
+		}
+		return one.totalOpusNum() == two.totalOpusNum();
+	}
+	
+	public String toString() 
+	{
+		StringBuilder sc = new StrinBuilder("").append(Ticket).append('\n');
+		if (Card.length == 0)
+		{
+			sc.append("\nNo OPUS Cards");
+		}
+		else
+			for (int i = 0; i < Card.length; i++)
+			{
+				sc.append("\n").append(Card[i].toString());
+			}
+	}
+	return sc.toString();			
+}	
 }
