f58fe41c905321f697a53090fcd1c1fb9e1f7ac8
kiwiinova <95432232+kiwiinova@users.noreply.github.com>
2021-12-05 00:25:35 -0500

    Update Ticketbooth.java

--- Ticketbooth.java
+++ Ticketbooth.java
@@ -3,29 +3,31 @@ import java.util.Scanner;
 public class Ticketbooth {
 	
 	private Tickets Ticket;
-	private OPUSCard[] OPUSCards;
+	private OPUSCard[] Card;
 	
-	Scanner sc = new Scanner(System.in);
-	int value = sc.nextInt();
-	
-	public Ticketbooth() {
-		Tickets obj = new Tickets();
-		OPUSCards = new OPUSCard[value];
-	}
-
-	public Ticketbooth(Tickets Ticket, OPUSCard card[]) 
+	public Ticketbooth() 
 	{
-		Tickets obj = new Tickets(Ticket);
-		card = null; 
+		Ticket = new Tickets(Ticket);
+		Card = new OPUSCard[0]; 
 	}
 
+	public Ticketbooth(Tickets Ticket, OPUSCard[] Card) 
+	{
+		Ticket = new Tickets(Ticket);
+		Card = new OPUSCard[Card.length]; 
+		for (int i = 0; i < Card.length; i++)
+		{
+			Card[i] = opusCard[i];
+		}
+	}
+	// method that verifies if the values of two ticketbooths are the same
 	public boolean equalValues(Ticketbooth T1, Ticketbooth T2) {
 		if (T1.Ticket.ticketsTotal() == T2.Ticket.ticketsTotal()) {
 			return true;
 		}
 		return false;
 	}
-	
+	// method that verifies if the number of each type of tickets of two ticketbooths are the same
 	public boolean equalNumber(Ticketbooth T1, Ticketbooth T2)
 	{
 		if (T1.Ticket.equals(Ticket) == T2.Ticket.equals(Ticket)) {
